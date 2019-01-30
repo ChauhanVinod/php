6867e4a04e7328d684293de526f94ffa77d08781
ChauhanVinod <vsc2137@yahoo.com>
2019-01-30 15:32:06 +0530

    new file created demo.php

---
+++ demo.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Demo</title>
+</head>
+<body>
+
+<h1>Demo for GIT </h1>
+<h3>If <br>else if <br>else if <br>else</h3>
+<?php
+	$a=10;
+	$b=10;
+	$c=30;
+	$d=40;
+	
+	if ($a == $b) {
+		echo "a and b are same";
+	}
+
+	else if ($a == $c) {
+		echo "a and c are same";
+
+	}
+
+	else if ($a == $d) {
+		echo "a and d are same";
+
+	}
+	else {
+		echo "NO same Value";
+	}
+
+	?>
+
+
+
+</body>
+</html>
